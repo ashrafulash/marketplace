394d5c690f4bab893e30b6a6d6a9ef88f2c93cab
ashrafulash <ashraful.freelance@gmail.com>
2019-11-07 16:46:29 +0600

    menu list

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+//fixed menu start
 (function(){
     
     
@@ -16,6 +18,54 @@
         
         
     });
+    
+    
+})();
+
+//product detail page > product infomation slider
+
+(function(){
+    $('.product_info_slider').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  nextArrow: '<i class="fas fa-angle-right detail_next_btn"></i>',
+  prevArrow: '<i class="fas fa-angle-left detail_prev_btn"></i>',
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+    
+    
+    
+
     
     
 })();
